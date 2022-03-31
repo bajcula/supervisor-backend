c882879e7c6996d72f6fca7e8c886bf5b1e8093a
bajculinho <97913469+Bajculinho@users.noreply.github.com>
2022-03-30 19:58:26 -0500

    added usercontr and model

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const workerController = require('./controllers/workerController')
+const userController = require('./controllers/userController')
 const app = express();
 const MONGOURI = process.env.MONGO_URI
 mongoose.connect(MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -14,12 +15,15 @@ db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: '));
 db.on('disconnected', () => console.log('mongo disconnected'));
 const Worker = require('./models/worker')
+const User = require('./models/user')
 
 app.use(morgan('short'))
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/workers', workerController)
+app.use('/users', userController)
+
 
 app.listen(3001, () => {
     console.log('app running...')
---
+++ controllers/userController.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const router = express()
+const User = require('../models/user')
+
+router.post('/', async(req,res)=>{
+    try{
+        const newUser = await User.create(req.body);
+        res.send({
+            success: true,
+            data: newUser
+        })
+    }catch(err){
+        res.send({
+            success: false,
+            data: err.message
+        })
+    }
+})
+
+router.post('/login', async(req,res)=>{
+    try{
+        const user = await User.findOne({email: req.body.email});
+        let err
+        if (!user){
+            let err = 'There is no such user in our database.'
+            res.send({
+                success:false,
+                data:err
+            })
+        } else {
+            if (user.password === req.body.password) {
+                res.send({
+                    success: true,
+                    data: user
+                })
+            } else {
+                let err = 'Wrong password.'
+                res.send({
+                    success:false,
+                    data:err
+                })
+            }
+        }
+    }catch(err){
+        res.send({
+            success: false,
+            data: err.message
+        })
+    }
+})
+
+router.put('/:id', async(req,res)=>{
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.send({
+            success: true,
+            data: user
+        })
+    }catch(err){
+        res.send({
+            success: false,
+            data: err.message
+        })
+    }
+})
+
+router.delete('/:id', async(req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        res.send({
+            success: true,
+            data: user
+        })
+    }catch(err){
+        res.send({
+            success: false,
+            data: err.message
+        })
+    }
+})
+
+module.exports = router;
