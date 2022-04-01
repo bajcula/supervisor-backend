3e6edd8284a8c820dd13ae8c803f678b64c0b6d9
bajculinho <97913469+Bajculinho@users.noreply.github.com>
2022-04-01 12:14:56 -0500

    cleaned comments

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: '));
 db.on('disconnected', () => console.log('mongo disconnected'));
-// const Worker = require('./models/worker')
-// const User = require('./models/user')
+// comment
 
 app.use(morgan('short'))
 app.use(cors())
